261de831d2c192c2232ef347c830e218d9bebc11
abid hussain (abidhissain12678-design) <abidhissain12678@gmail.com>
2026-04-09 15:48:48 +0500

    Added app options for CORS preflight

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,6 +7,7 @@ import taskRoutes from './routes/taskRoutes';
 
 const app = express();
 app.use(cors({
+  
   origin: [
     "http://localhost:3000",
     "https://rs-10-convert-one-million.vercel.app"
@@ -14,7 +15,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
   credentials: true
 }));
-
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/api/auth', authRoutes);
